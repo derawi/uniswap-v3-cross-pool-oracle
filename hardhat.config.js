cc207d462c39f0704e9200c5219ac2a7354eb545
derawi <deraw@protonmail.com>
2022-08-12 15:05:50 +0200

    Add goerli network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,13 +1,13 @@
 require('@nomiclabs/hardhat-ethers')
 require('@nomiclabs/hardhat-etherscan')
-require('hardhat-local-networks-config-plugin')
+// require('hardhat-local-networks-config-plugin')
 require('dotenv').config()
 
 console.log(process.env.RPC_URL_MAINNET)
 
 module.exports = {
   solidity: {
-    version: '0.8.16',
+    version: '0.8.12',
     settings: {
       optimizer: {
         enabled: true,
@@ -23,6 +23,10 @@ module.exports = {
     },
     mainnet: {
       url: process.env.RPC_URL_MAINNET,
+    },
+    goerli: {
+      url: process.env.RPC_URL_GOERLI2,
+      accounts: [process.env.PRIVATE_KEY2],
     },
   },
   etherscan: {
